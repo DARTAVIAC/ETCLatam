f715bf779f35ddc6fdf3d87cc4adc088b8c10110
Diego Artavia <71478240+DARTAVIAC@users.noreply.github.com>
2022-11-13 16:04:54 -0600

    Add files via upload

---
+++ Backend/src/main/java/com/sistema/prueba/repositorios/RolRepository.java
@@ -0,0 +1,7 @@
+package com.sistema.prueba.repositorios;
+
+import com.sistema.prueba.modelo.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol,Long> {
+}
---
+++ Backend/src/main/java/com/sistema/prueba/servicios/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.sistema.prueba.servicios;
+
+import com.sistema.prueba.modelo.Usuario;
+import com.sistema.prueba.modelo.UsuarioRol;
+
+import java.util.Set;
+
+public interface UsuarioService {
+
+    public Usuario guardarUsuario(Usuario usuario, Set<UsuarioRol> usuarioRoles) throws Exception;
+
+    public Usuario obtenerUsuario(String username);
+
+    public void eliminarUsuario(Long usuarioId);
+}
